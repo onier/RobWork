872cf66bbc2518e358ea75b0e77c73afe59215f7
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2010-06-02 22:49:37 +0000

    Updated for visual studio

--- RobWorkStudio/src/rws/lua/LuaRWStudioStub.hpp
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: LuaRWStudio
-** Generated automatically by tolua++-1.0.92 on 06/02/10 20:53:45.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_LuaRWStudio_open (lua_State* tolua_S);
-
