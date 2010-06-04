2ad463ada202212bfda4802602a620e04a58c110
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2010-06-04 09:48:11 +0000

    - Updated headers and API doc in RobWorkHardware
    - Updated readme file in RobWorkHardware

--- RobWorkHardware/src/rwhw/camera/DC1394CameraFactory.hpp
+++ RobWorkHardware/src/rwhw/camera/DC1394CameraFactory.hpp
@@ -1,9 +1,19 @@
-/*
- * DC1394CameraFactory.h
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
  *
- *  Created on: Feb 3, 2010
- *      Author: dr
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
 
 #ifndef RWHW_CAMERA_DC1394CAMERAFACTORY_HPP
 #define RWHW_CAMERA_DC1394CAMERAFACTORY_HPP
@@ -23,10 +33,24 @@ namespace camera {
 
     //class DC1394Camera : public rw::sensor::CameraFirewire {};
 
+    /**
+     * @brief Factory for DC1394 Cameras
+     */
     class DC1394CameraFactory {
     public:
+        /**
+         * @brief Returns instance of CD1394CameraFactory
+         */
         static DC1394CameraFactory* getInstance();
+
+        /**
+         * @brief Returns lists of cameras
+         */
         const std::vector<std::string>& getCameraList();
+
+        /**
+         * @brief Returns a camera associated to a specific frame and a given index
+         */
         DC1394Camera* getCamera(rw::kinematics::Frame* frame, unsigned int index);
         void freeCamera(DC1394Camera* camera);
 
